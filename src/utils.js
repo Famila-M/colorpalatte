b1eceb66070236d9141098b910011eac0f2b8c31
Famila-M <familafami99@outlook.com>
2021-07-27 19:33:43 +0530

    commit

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+/* eslint-disable eqeqeq */
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? '0' + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+export default rgbToHex;
